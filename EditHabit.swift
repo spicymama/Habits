c184542476ca91ee5aae47b1e5e3a55ea1021668
spicymama <gavinwoffinden@yahoo.com>
2022-12-01 14:50:19 -0700

    reminder times save and display when editing a goal

--- EditHabit.swift
+++ EditHabit.swift
@@ -32,6 +32,8 @@ struct EditHabit: View {
     @State var satReminders: [Date] = []
     @State var sunReminders: [Date] = []
     
+    let timeFormatter = DateFormatter()
+    
     var body: some View {
         
         VStack {
@@ -49,177 +51,254 @@ struct EditHabit: View {
 
             }
             .frame(maxWidth: UIScreen.main.bounds.width - 30, maxHeight: 65, alignment: .leading)
-            HStack {
-                Text("M  ")
-                    .onTapGesture {
-                        self.monHidden.toggle()
-                    }
-                self.monHidden ? nil : DatePicker("M :", selection: $monDate, displayedComponents: .hourAndMinute)
-                    .frame(width: 150, height: 55)
-                    .labelsHidden()
-                Button {
-                    print(self.monReminders)
-                    if self.monHidden == true {
-                        self.monHidden.toggle()
-                    } else {
-                        if !self.monReminders.contains(monDate) {
-                            self.monReminders.append(monDate)
+            Group {
+                HStack {
+                    Text("M  ")
+                        .onTapGesture {
+                            self.monHidden.toggle()
                         }
-                    }
-                    
-                } label: {
-                    Image(systemName: "plus.square")
-                        .foregroundColor(.black)
-                        .imageScale(.large)
-                }
-            }
-            .frame(maxWidth: UIScreen.main.bounds.width - 60, maxHeight: 35, alignment: .leading)
-            .padding(.bottom, 10)
-           
-            HStack {
-                Text("T   ")
-                    .onTapGesture {
-                        self.tusHidden.toggle()
-                    }
-                self.tusHidden ? nil :  DatePicker("T :", selection: $tusDate, displayedComponents: .hourAndMinute)
-                    .frame(width: 150, height: 55)
-                    .labelsHidden()
-                Button {
-                    if self.tusHidden == true {
-                        self.tusHidden.toggle()
-                    } else {
-                        if !self.tusReminders.contains(tusDate) {
-                            self.tusReminders.append(tusDate)
+                    self.monHidden ? nil : DatePicker("M :", selection: $monDate, displayedComponents: .hourAndMinute)
+                        .frame(width: 150, height: 55)
+                        .labelsHidden()
+                    Button {
+                        print(self.monReminders)
+                        if self.monHidden == true {
+                            self.monHidden.toggle()
+                        } else {
+                            if !self.monReminders.contains(monDate) {
+                                self.monReminders.append(monDate)
+                            }
                         }
+                        
+                    } label: {
+                        Image(systemName: "plus.square")
+                            .foregroundColor(.black)
+                            .imageScale(.large)
                     }
-                } label: {
-                    Image(systemName: "plus.square")
-                        .foregroundColor(.black)
-                        .imageScale(.large)
                 }
-            }
-            .frame(maxWidth: UIScreen.main.bounds.width - 60, maxHeight: 35, alignment: .leading)
-            .padding(.bottom, 10)
-            HStack {
-                Text("W  ")
-                    .onTapGesture {
-                        self.wedHidden.toggle()
+                .frame(maxWidth: UIScreen.main.bounds.width - 60, maxHeight: 35, alignment: .leading)
+                .padding(.bottom, 10)
+                HStack {
+                    ForEach(showSelectedTimes(dates: self.monReminders)) { time in
+                        Text(time)
+                            .foregroundColor(.gray)
+                            .font(.system(size: 12))
                     }
-                self.wedHidden ? nil : DatePicker("W :", selection: $wedDate, displayedComponents: .hourAndMinute)
-                    .frame(width: 150, height: 55)
-                    .labelsHidden()
-                Button {
-                    if self.wedHidden == true {
-                        self.wedHidden.toggle()
-                    } else {
-                        if !self.wedReminders.contains(wedDate) {
-                            self.wedReminders.append(wedDate)
+                    .padding(.trailing, 5)
+                }
+                .frame(maxWidth: UIScreen.main.bounds.width - 60, alignment: .leading)
+            }
+            Group {
+                HStack {
+                    Text("T   ")
+                        .onTapGesture {
+                            self.tusHidden.toggle()
                         }
-                    }
-                } label: {
-                    Image(systemName: "plus.square")
-                        .foregroundColor(.black)
-                        .imageScale(.large)
-                }
-            }
-            .frame(maxWidth: UIScreen.main.bounds.width - 60, maxHeight: 35, alignment: .leading)
-            .padding(.bottom, 10)
-            HStack {
-                Text("Th  ")
-                    .onTapGesture {
-                        self.thursHidden.toggle()
-                    }
-                self.thursHidden ? nil : DatePicker("Th :", selection: $thursDate, displayedComponents: .hourAndMinute)
-                    .frame(width: 150, height: 55)
-                    .labelsHidden()
-                Button {
-                    if self.thursHidden == true {
-                        self.thursHidden.toggle()
-                    } else {
-                        if !self.thursReminders.contains(thursDate) {
-                            self.thursReminders.append(thursDate)
+                    self.tusHidden ? nil :  DatePicker("T :", selection: $tusDate, displayedComponents: .hourAndMinute)
+                        .frame(width: 150, height: 55)
+                        .labelsHidden()
+                    Button {
+                        if self.tusHidden == true {
+                            self.tusHidden.toggle()
+                        } else {
+                            if !self.tusReminders.contains(tusDate) {
+                                self.tusReminders.append(tusDate)
+                            }
                         }
+                    } label: {
+                        Image(systemName: "plus.square")
+                            .foregroundColor(.black)
+                            .imageScale(.large)
                     }
-                } label: {
-                    Image(systemName: "plus.square")
-                        .foregroundColor(.black)
-                        .imageScale(.large)
                 }
-            }
-            .frame(maxWidth: UIScreen.main.bounds.width - 60, maxHeight: 35, alignment: .leading)
-            .padding(.bottom, 10)
-            HStack {
-                Text("F    ")
-                    .onTapGesture {
-                        self.friHidden.toggle()
-                    }
-                self.friHidden ? nil : DatePicker("F :", selection: $friDate, displayedComponents: .hourAndMinute)
-                    .frame(width: 150, height: 55)
-                    .labelsHidden()
-                Button {
-                    if self.friHidden == true {
-                        self.friHidden.toggle()
-                    } else {
-                        if !self.friReminders.contains(friDate) {
-                            self.friReminders.append(friDate)
+                .frame(maxWidth: UIScreen.main.bounds.width - 60, maxHeight: 35, alignment: .leading)
+                .padding(.bottom, 10)
+                HStack {
+                        ForEach(showSelectedTimes(dates: self.tusReminders)) { time in
+                                Text(time)
+                                    .foregroundColor(.gray)
+                                    .font(.system(size: 12))
                         }
-                    }
-                } label: {
-                    Image(systemName: "plus.square")
-                        .foregroundColor(.black)
-                        .imageScale(.large)
+                        .padding(.trailing, 5)
                 }
+                .frame(maxWidth: UIScreen.main.bounds.width - 60, alignment: .leading)
             }
-            .frame(maxWidth: UIScreen.main.bounds.width - 60, maxHeight: 35, alignment: .leading)
-            .padding(.bottom, 10)
-            HStack {
-                Text("Sat ")
-                    .onTapGesture {
-                        self.satHidden.toggle()
-                    }
-                self.satHidden ? nil : DatePicker("Sat :", selection: $satDate, displayedComponents: .hourAndMinute)
-                    .frame(width: 150, height: 55)
-                    .labelsHidden()
-                Button {
-                    if self.satHidden == true {
-                        self.satHidden.toggle()
-                    } else {
-                        if !self.satReminders.contains(satDate) {
-                            self.satReminders.append(satDate)
+            Group {
+                HStack {
+                    Text("W  ")
+                        .onTapGesture {
+                            self.wedHidden.toggle()
                         }
-                    }
-                } label: {
-                    Image(systemName: "plus.square")
-                        .foregroundColor(.black)
-                        .imageScale(.large)
-                }
-            }
-            .frame(maxWidth: UIScreen.main.bounds.width - 60, maxHeight: 35, alignment: .leading)
-            .padding(.bottom, 10)
-            HStack {
-                Text("Sun")
-                    .onTapGesture {
-                        self.sunHidden.toggle()
-                    }
-                self.sunHidden ? nil : DatePicker("Sun :", selection: $sunDate, displayedComponents: .hourAndMinute)
-                    .frame(width: 150, height: 55)
-                    .labelsHidden()
-                Button {
-                    if self.sunHidden == true {
-                        self.sunHidden.toggle()
-                    } else {
-                        if !self.sunReminders.contains(sunDate) {
-                            self.sunReminders.append(sunDate)
+                    self.wedHidden ? nil : DatePicker("W :", selection: $wedDate, displayedComponents: .hourAndMinute)
+                        .frame(width: 150, height: 55)
+                        .labelsHidden()
+                    Button {
+                        if self.wedHidden == true {
+                            self.wedHidden.toggle()
+                        } else {
+                            if !self.wedReminders.contains(wedDate) {
+                                self.wedReminders.append(wedDate)
+                            }
                         }
+                    } label: {
+                        Image(systemName: "plus.square")
+                            .foregroundColor(.black)
+                            .imageScale(.large)
                     }
-                } label: {
-                    Image(systemName: "plus.square")
-                        .foregroundColor(.black)
-                        .imageScale(.large)
                 }
+                .frame(maxWidth: UIScreen.main.bounds.width - 60, maxHeight: 35, alignment: .leading)
+                .padding(.bottom, 10)
+                HStack {
+                        ForEach(showSelectedTimes(dates: self.wedReminders)) { time in
+                                Text(time)
+                                    .foregroundColor(.gray)
+                                    .font(.system(size: 12))
+                        }
+                        .padding(.trailing, 5)
+                }
+                .frame(maxWidth: UIScreen.main.bounds.width - 60, alignment: .leading)
             }
-            .frame(maxWidth: UIScreen.main.bounds.width - 60, maxHeight: 35, alignment: .leading)
-            .padding(.bottom, 10)
+            Group {
+                HStack {
+                    Text("Th  ")
+                        .onTapGesture {
+                            self.thursHidden.toggle()
+                        }
+                    self.thursHidden ? nil : DatePicker("Th :", selection: $thursDate, displayedComponents: .hourAndMinute)
+                        .frame(width: 150, height: 55)
+                        .labelsHidden()
+                    Button {
+                        if self.thursHidden == true {
+                            self.thursHidden.toggle()
+                        } else {
+                            if !self.thursReminders.contains(thursDate) {
+                                self.thursReminders.append(thursDate)
+                            }
+                        }
+                    } label: {
+                        Image(systemName: "plus.square")
+                            .foregroundColor(.black)
+                            .imageScale(.large)
+                    }
+                }
+                .frame(maxWidth: UIScreen.main.bounds.width - 60, maxHeight: 35, alignment: .leading)
+                .padding(.bottom, 10)
+                HStack {
+                        ForEach(showSelectedTimes(dates: self.thursReminders)) { time in
+                                Text(time)
+                                    .foregroundColor(.gray)
+                                    .font(.system(size: 12))
+                        }
+                        .padding(.trailing, 5)
+                }
+                .frame(maxWidth: UIScreen.main.bounds.width - 60, alignment: .leading)
+            }
+            Group {
+                HStack {
+                    Text("F    ")
+                        .onTapGesture {
+                            self.friHidden.toggle()
+                        }
+                    self.friHidden ? nil : DatePicker("F :", selection: $friDate, displayedComponents: .hourAndMinute)
+                        .frame(width: 150, height: 55)
+                        .labelsHidden()
+                    Button {
+                        if self.friHidden == true {
+                            self.friHidden.toggle()
+                        } else {
+                            if !self.friReminders.contains(friDate) {
+                                self.friReminders.append(friDate)
+                            }
+                        }
+                    } label: {
+                        Image(systemName: "plus.square")
+                            .foregroundColor(.black)
+                            .imageScale(.large)
+                    }
+                }
+                .frame(maxWidth: UIScreen.main.bounds.width - 60, maxHeight: 35, alignment: .leading)
+                .padding(.bottom, 10)
+                HStack {
+                        ForEach(showSelectedTimes(dates: self.friReminders)) { time in
+                                Text(time)
+                                    .foregroundColor(.gray)
+                                    .font(.system(size: 12))
+                        }
+                        .padding(.trailing, 5)
+                }
+                .frame(maxWidth: UIScreen.main.bounds.width - 60, alignment: .leading)
+            }
+            Group {
+                HStack {
+                    Text("Sat ")
+                        .onTapGesture {
+                            self.satHidden.toggle()
+                        }
+                    self.satHidden ? nil : DatePicker("Sat :", selection: $satDate, displayedComponents: .hourAndMinute)
+                        .frame(width: 150, height: 55)
+                        .labelsHidden()
+                    Button {
+                        if self.satHidden == true {
+                            self.satHidden.toggle()
+                        } else {
+                            if !self.satReminders.contains(satDate) {
+                                self.satReminders.append(satDate)
+                            }
+                        }
+                    } label: {
+                        Image(systemName: "plus.square")
+                            .foregroundColor(.black)
+                            .imageScale(.large)
+                    }
+                }
+                .frame(maxWidth: UIScreen.main.bounds.width - 60, maxHeight: 35, alignment: .leading)
+                .padding(.bottom, 10)
+                HStack {
+                        ForEach(showSelectedTimes(dates: self.satReminders)) { time in
+                                Text(time)
+                                    .foregroundColor(.gray)
+                                    .font(.system(size: 12))
+                        }
+                        .padding(.trailing, 5)
+                }
+                .frame(maxWidth: UIScreen.main.bounds.width - 60, alignment: .leading)
+            }
+            Group {
+                HStack {
+                    Text("Sun")
+                        .onTapGesture {
+                            self.sunHidden.toggle()
+                        }
+                    self.sunHidden ? nil : DatePicker("Sun :", selection: $sunDate, displayedComponents: .hourAndMinute)
+                        .frame(width: 150, height: 55)
+                        .labelsHidden()
+                    Button {
+                        if self.sunHidden == true {
+                            self.sunHidden.toggle()
+                        } else {
+                            if !self.sunReminders.contains(sunDate) {
+                                self.sunReminders.append(sunDate)
+                            }
+                        }
+                    } label: {
+                        Image(systemName: "plus.square")
+                            .foregroundColor(.black)
+                            .imageScale(.large)
+                    }
+                }
+                .frame(maxWidth: UIScreen.main.bounds.width - 60, maxHeight: 35, alignment: .leading)
+                .padding(.bottom, 10)
+                HStack {
+                        ForEach(showSelectedTimes(dates: self.sunReminders)) { time in
+                                Text(time)
+                                    .foregroundColor(.gray)
+                                    .font(.system(size: 12))
+                        }
+                        .padding(.trailing, 5)
+                }
+                .frame(maxWidth: UIScreen.main.bounds.width - 60, alignment: .leading)
+            }
+            
             HStack {
                 Text("End Date")
                 Button {
@@ -236,10 +315,22 @@ struct EditHabit: View {
         }
         .frame(maxWidth: UIScreen.main.bounds.width - 30, maxHeight: 5000, alignment: .leading)
     }
+    func showSelectedTimes(dates: [Date]) -> [String] {
+        var timeArr: [String] = [""]
+        for date in dates.sorted(by: { $0.compare($1) == .orderedAscending }) {
+            timeFormatter.timeStyle = .short
+            timeArr.append(timeFormatter.string(from: date))
+        }
+        return timeArr
+    }
 }
 
 struct EditHabit_Previews: PreviewProvider {
     static var previews: some View {
         EditHabit()
     }
+}
+
+extension String: Identifiable {
+    public var id: String { self }
 }
--- Home.swift
+++ Home.swift
@@ -12,32 +12,34 @@ struct Home: View {
     @State var tap = ListTile.shared.wasTapped
     
     var body: some View {
-        ScrollView {
-            VStack {
+        NavigationStack {
+            ScrollView {
                 VStack {
-                    Button {
-                        
-                    } label: {
-                        Label("", systemImage: "plus.square")
-                            .imageScale(.large)
-                            .foregroundColor(.gray)
-                    }
-                    .frame(maxWidth: 15, maxHeight: 15, alignment: .topTrailing)
-                    .padding(.leading, UIScreen.main.bounds.width - 65)
-                    
-                    Text("Habits")
-                        .frame(maxWidth: 250, maxHeight: 55, alignment: .top)
-                        .font(.system(size: 30))
+                    VStack {
+                        NavigationLink {
+                            EditHabit()
+                        } label: {
+                            Image(systemName: "plus.square")
+                                .imageScale(.large)
+                        }
+                        .frame(maxWidth: 15, maxHeight: 15, alignment: .topTrailing)
+                        .padding(.leading, UIScreen.main.bounds.width - 65)
                         .foregroundColor(.gray)
-                        .padding(.bottom, 25)
+                        
+                        Text("Habits")
+                            .frame(maxWidth: 250, maxHeight: 55, alignment: .top)
+                            .font(.system(size: 30))
+                            .foregroundColor(.gray)
+                            .padding(.bottom, 25)
+                        
+                    }
+                    
+                    ListTile()
+                    
+                    ListTile()
+                    
                     
                 }
-                
-                ListTile()
-                    
-                ListTile()
-                  
-                   
             }
         }
     }
